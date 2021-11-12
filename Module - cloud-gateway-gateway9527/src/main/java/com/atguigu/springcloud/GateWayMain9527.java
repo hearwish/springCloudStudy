4939d5ac63e8121521025878ffbee7f9d400966d
yuzy <yumeiqun@qq.com>
2021-11-13 02:12:12 +0800

    v1.0 yuzy_dev (9527 GateWay) 第一种配置

---
+++ Module - cloud-gateway-gateway9527/src/main/java/com/atguigu/springcloud/GateWayMain9527.java
@@ -0,0 +1,21 @@
+package com.atguigu.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+/**
+ * @ClassName GateWayMain9527
+ * @Description TODO
+ * @Author yzy
+ * @Date 2021/11/13 1:51 上午
+ * @Version 1.0
+ */
+@SpringBootApplication
+@EnableEurekaClient
+public class GateWayMain9527
+{
+    public static void main(String[] args) {
+        SpringApplication.run(GateWayMain9527.class, args);
+    }
+}
