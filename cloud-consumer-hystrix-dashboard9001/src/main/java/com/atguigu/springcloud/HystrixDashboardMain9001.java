46bf786945106cf5d296ed899baac643b055e314
yuzy <yumeiqun@qq.com>
2021-11-12 23:59:44 +0800

    v1.0 yuzy_dev (9001 HystrixDashboard) Hystrixdashboard 演示图形化界面 1.module中 pom需要actuator依赖 主启动类加注解:@EnableHystrixDashboard 2.被检测module中 需要配置相关的Hystrix应用注解

---
+++ cloud-consumer-hystrix-dashboard9001/src/main/java/com/atguigu/springcloud/HystrixDashboardMain9001.java
@@ -0,0 +1,21 @@
+package com.atguigu.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
+
+/**
+ * @ClassName HystrixDashboardMain9001
+ * @Description TODO
+ * @Author yzy
+ * @Date 2021/11/12 11:30 下午
+ * @Version 1.0
+ */
+@SpringBootApplication
+@EnableHystrixDashboard
+public class HystrixDashboardMain9001
+{
+    public static void main(String[] args) {
+        SpringApplication.run(HystrixDashboardMain9001.class, args);
+    }
+}
